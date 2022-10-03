ea24efc8c9679631e1d34f451bd6db260043974c
henriquekato <hshojikato@gmail.com>
2022-10-02 21:27:26 -0300

    create json fields

---
+++ ContentJson.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class AuthJson
+{
+    public string code;
+    public string token;
+    public int id;
+}
+
+[Serializable]
+public class RequestCreateJson
+{
+    public string code;
+    public int id;
+}
+
+[Serializable]
+public class RequestListJson
+{
+    public string code;
+    public _list[] list;
+    public int count;
+}
+
+[Serializable]
+public class _list
+{
+    public int id;
+    public int key;
+    public int status;
+    public string date_expected_start;
+    public string date_expected_end;
+    public string user_name;
+}
+
+[Serializable]
+public class RequestUpdateJson
+{
+    public string code;
+}
+
+[Serializable]
+public class EditUserJson
+{
+    public string code;
+}
