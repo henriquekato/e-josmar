6ee8b2525a9b894dee042b3cb57fe4f9d87cbf67
henriquekato <hshojikato@gmail.com>
2022-10-08 11:59:22 -0300

    reorganize

--- OpenPanelButton.cs
+++ OpenPanelButton.cs
@@ -11,6 +11,13 @@ public class OpenPanelButton : MonoBehaviour
     [SerializeField] GameObject panelUpdateRequest;
     [SerializeField] Text txtThisRoom;
     [SerializeField] Text txtNextRoom;
+    [SerializeField] Dropdown dpdStartTime;
+    [SerializeField] InputField inputStartHour;
+    [SerializeField] InputField inputStartMin;
+    [SerializeField] Dropdown dpdEndTime;
+    [SerializeField] InputField inputEndHour;
+    [SerializeField] InputField inputEndMin;
+    [SerializeField] Dropdown dpdWeekDay;
     [SerializeField] Dropdown dpdRequestsList;
     [SerializeField] GameObject panelMsg;
     [SerializeField] Text txtMsg;
@@ -22,6 +29,8 @@ public class OpenPanelButton : MonoBehaviour
         string sKey = txtThisRoom.text.Substring(1);
         txtNextRoom.text = txtThisRoom.text.Substring(0, 1) == "S" ? "Sala: " + sKey : "Laboratório: " + sKey;
         
+        Utilities.ClearFields(dpdStartTime, dpdEndTime, inputStartHour, inputStartMin, inputEndHour, inputEndMin, dpdWeekDay);
+        
         Utilities.UpdateDropdownList(dpdRequestsList, sKey);
 
         panelCover.SetActive(true);
@@ -32,6 +41,8 @@ public class OpenPanelButton : MonoBehaviour
     {
         string sKey = txtThisRoom.text.Substring(0, 1) == "S" ? txtThisRoom.text.Substring(6) : txtThisRoom.text.Substring(13);
         txtNextRoom.text = txtThisRoom.text.Substring(0, 1) == "S" ? "Sala: " + sKey : "Laboratório: " + sKey;
+
+        Utilities.ClearFields(dpdStartTime, dpdEndTime, inputStartHour, inputStartMin, inputEndHour, inputEndMin, dpdWeekDay);
 
         Utilities.UpdateDropdownList(dpdRequestsList, sKey);
 
