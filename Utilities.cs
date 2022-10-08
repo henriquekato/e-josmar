410e918f7e55dd4e5a972aaeb4dd61d49bc24e92
henriquekato <hshojikato@gmail.com>
2022-10-08 11:20:12 -0300

    add functions in utilities

--- Utilities.cs
+++ Utilities.cs
@@ -11,9 +11,9 @@ public class Utilities
     public enum Status
     {
         not_started = 1,
-        askToStarted = 2,
+        start_request = 2,
         started = 3,
-        askToEnded = 4,
+        end_request = 4,
         ended = 5,
         canceled = 6
     }
@@ -61,12 +61,12 @@ public class Utilities
         {
             if(Success)
             {
-                if(Status == (int)Utilities.Status.started)
+                if(Status == (int)Utilities.Status.start_request)
                 {
                     TxtStatus.text = "Status: começado";
                     BtnReturnKey.interactable = true;
                 }
-                else if(Status == (int)Utilities.Status.canceled | Status == (int)Utilities.Status.ended)
+                else if(Status == (int)Utilities.Status.canceled | Status == (int)Utilities.Status.end_request)
                 {
                     TxtStatus.text = "Status: finalizado";
                 }
@@ -84,5 +84,51 @@ public class Utilities
                 }
             }
         }
+    }
+
+    public static void UpdateDropdownList(Dropdown DpdRequestsList, string SKey)
+    {
+        DpdRequestsList.interactable = false;
+        DpdRequestsList.options.Clear();
+        DpdRequestsList.options.Add(new Dropdown.OptionData("Pedidos"));
+        foreach(Key key in User.user.UserKeys)
+        {
+            if(key.roomNumber.ToString() == SKey)
+            {
+                DpdRequestsList.options.Add(new Dropdown.OptionData("Pedido " + key.requestId.ToString()));
+            }
+        }
+        DpdRequestsList.value = 0;
+        DpdRequestsList.RefreshShownValue();
+        DpdRequestsList.interactable = true;
+    }
+
+    public static void ClearFields(Dropdown DpdStartTime, Dropdown DpdEndTime, InputField InputStartHour, InputField InputStartMin, InputField InputEndHour, InputField InputEndMin, Dropdown DpdWeekDay)
+    {
+        DpdStartTime.value = 0;
+        // DpdStartTime.RefreshShownValue();
+        DpdEndTime.value = 0;
+        // DpdEndTime.RefreshShownValue();
+        InputStartHour.text = "";
+        InputStartMin.text = "";
+        InputEndHour.text = "";
+        InputEndMin.text = "";
+        DpdWeekDay.value = 0;
+        // DpdWeekDay.RefreshShownValue();
+    }
+
+    public static bool TimeOk(Key key)
+    {
+        int iYear, iMonth, iDay, iHour, iMin, iSec;
+        Int32.TryParse(key.dateStart.Substring(0, 4), out iYear);
+        Int32.TryParse(key.dateStart.Substring(5, 2), out iMonth);
+        Int32.TryParse(key.dateStart.Substring(8, 2), out iDay);
+        Int32.TryParse(key.dateStart.Substring(11, 2), out iHour);
+        Int32.TryParse(key.dateStart.Substring(14, 2), out iMin);
+        Int32.TryParse(key.dateStart.Substring(17, 2), out iSec);
+        DateTime keyDateStart = new DateTime(iYear, iMonth, iDay, iHour, iMin, iSec);
+
+        bool isOk = DateTime.Now < keyDateStart ? false : true;
+        return isOk;
     }
 }
