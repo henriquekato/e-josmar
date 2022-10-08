8478efe472c067276ab7c72ccc4865edd142f27c
henriquekato <hshojikato@gmail.com>
2022-10-08 11:22:07 -0300

    fix status

--- AuthButton.cs
+++ AuthButton.cs
@@ -63,7 +63,7 @@ public class AuthButton : MonoBehaviour
 
     IEnumerator GetLoadUserKeys()
     {
-        UnityWebRequest requestLoadUserKeys = UnityWebRequest.Get(Utilities.apiURL + "/api/request/list?user=" + User.user.UserId + "&status=" + ((int)Utilities.Status.not_started).ToString() + "|" + ((int)Utilities.Status.started).ToString() + "&date_start=" + DateTime.UtcNow.ToLocalTime().ToString("yyyy-MM-dd") + " 00:00:00" + "&date_end=" + DateTime.UtcNow.ToLocalTime().ToString("yyyy") + "-12-31 23:59:59" + "&token=" + User.user.UserToken);
+        UnityWebRequest requestLoadUserKeys = UnityWebRequest.Get(Utilities.apiURL + "/api/request/list?user=" + User.user.UserId + "&status=" + ((int)Utilities.Status.not_started).ToString() + "|" + ((int)Utilities.Status.started).ToString() + "&date_start=" + DateTime.Now.ToString("yyyy-MM-dd") + " 00:00:00" + "&date_end=" + DateTime.Now.ToString("yyyy") + "-12-31 23:59:59" + "&token=" + User.user.UserToken);
         yield return requestLoadUserKeys.SendWebRequest();
 
         if(requestLoadUserKeys.result == UnityWebRequest.Result.ConnectionError | requestLoadUserKeys.result == UnityWebRequest.Result.ProtocolError)
