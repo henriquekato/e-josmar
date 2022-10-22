b19fbfcab3cfda3d0a896f21eee953b0f62f7423
henriquekato <hshojikato@gmail.com>
2022-10-22 17:29:10 -0300

    close all panels

--- ClosePanelButton.cs
+++ ClosePanelButton.cs
@@ -6,6 +6,10 @@ public class ClosePanelButton : MonoBehaviour
     [SerializeField] GameObject panelCover;
     [SerializeField] GameObject panelRequest;
     [SerializeField] GameObject panelUpdateRequest;
+    [SerializeField] GameObject panelCoverConfirm;
+    [SerializeField] GameObject panelConfirm;
+    [SerializeField] GameObject panelCoverAllRequests;
+    [SerializeField] GameObject panelAllRequest;
     [SerializeField] GameObject panelMsg;
 
     public void ClosePanel()
@@ -13,6 +17,10 @@ public class ClosePanelButton : MonoBehaviour
         panelCover.SetActive(false);
         panelRequest.SetActive(false);
         panelUpdateRequest.SetActive(false);
+        panelCoverConfirm.SetActive(false);
+        panelConfirm.SetActive(false);
+        panelCoverAllRequests.SetActive(false);
+        panelAllRequests.SetActive(false);
         panelMsg.SetActive(false);
     }
 }
