05daf8fb77955bfbcf8695b79f1893b9351d2a96
henriquekato <hshojikato@gmail.com>
2022-10-22 17:08:23 -0300

    remove usused namespace

--- EditUserButton.cs
+++ EditUserButton.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
 public class EditUserButton : MonoBehaviour
