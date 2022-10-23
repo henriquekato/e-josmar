181678bc38f0e0ce17dd20804f5f3f715e02d476
henriquekato <hshojikato@gmail.com>
2022-10-23 15:41:00 -0300

    rename

--- OpenAllRequestsButton.cs
+++ OpenAllRequestsButton.cs
@@ -3,17 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
 
-public class AllRequestsButton : MonoBehaviour
+public class OpenAllRequestsButton : MonoBehaviour
 {
     [SerializeField] Dropdown dpdRequestsList;
     [SerializeField] GameObject panelCover;
     [SerializeField] GameObject panelAllRequests;
 
-    public void AllRequests()
+    public void OpenAllRequests()
     {
         Utilities.UpdateDropdownAllRequests(dpdRequestsList);
+        dpdRequestsList.Show();
         panelCover.SetActive(true);
         panelAllRequests.SetActive(true);
     }
