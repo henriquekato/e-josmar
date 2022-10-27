dbab4208210e9ba3ac05a476a6b7f79f82e7b915
henriquekato <hshojikato@gmail.com>
2022-10-27 18:46:57 -0300

    msg

--- OpenAllRequestsButton.cs
+++ OpenAllRequestsButton.cs
@@ -9,10 +9,13 @@ public class OpenAllRequestsButton : MonoBehaviour
     [SerializeField] Dropdown dpdRequestsList;
     [SerializeField] GameObject panelCover;
     [SerializeField] GameObject panelAllRequests;
+    [SerializeField] GameObject panelMsg;
+    [SerializeField] Text txtMsg;
 
     public void OpenAllRequests()
     {
         Utilities.UpdateDropdownAllRequests(dpdRequestsList);
+        Utilities.ClearFields(TxtMsg:txtMsg, PanelMsg:panelMsg);
         panelCover.SetActive(true);
         panelAllRequests.SetActive(true);
     }
